58c946dc74a016e48bc611e0afc5316ed9638525
shreyas thombre <shreyas.thombre@gmail.com>
2020-01-21 21:07:59 -0500

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age, height,heartrate;
+            age = 23;
+            height = 175;
+
+            //max heart rate = 220-age
+
+            heartrate = 220 - age;
+
+            Console.WriteLine("I am " +age+ " years old, my max heart rate is "+heartrate+",I am "+height+" tall.");
+            Console.ReadLine();
+        }
+    }
+}
